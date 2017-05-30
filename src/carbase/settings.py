917390c09a0d77d013e0af167318ccac3bf3a2fb
Nurzhan Nazarov <nurzhan@allpri.me>
2017-05-30 11:19:28 +0600

    add pki app

--- src/carbase/settings.py
+++ src/carbase/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'cars.apps.CarsConfig'
+    'cars.apps.CarsConfig',
+    'pki.apps.PkiConfig'
 ]
 
 MIDDLEWARE = [
