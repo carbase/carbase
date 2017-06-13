b350f65c32f7e78a28bd2387a827c29c8ed321bb
danikarik <baitursynov92@gmail.com>
2017-06-13 14:57:06 +0600

    removed unnecessary functions in helper module

--- src/apps/payment/api.py
+++ src/apps/payment/api.py
@@ -44,29 +44,29 @@ def do_request(url, params):
 
 def get_checkout_url(parameters):
 
-    amount          = parameters.get('amount', 0)
-    description     = parameters.get('order_desc', '')
-    order_id        = '{}:{}'.format(parameters.get('product_id', ''), create_guid())
-    merchant_id     = settings.PAYBOX['MERCHANT_ID']
-    secret_key      = settings.PAYBOX['SECRET_KEY']
-    payment_url     = settings.PAYBOX['PAYMENT_URL']
-    result_url      = settings.PAYBOX['RESULT_URL']
-    success_url     = settings.PAYBOX['SUCCESS_URL']
-    failure_url     = settings.PAYBOX['FAILURE_URL']
-    testing_mode    = settings.PAYBOX['TESTING_MODE']
-    script          = urlparse(payment_url).path[1:]
-    salt            = settings.SECRET_KEY
+    amount = parameters.get('amount', 0)
+    description = parameters.get('order_desc', '')
+    order_id = '{}:{}'.format(parameters.get('product_id', ''), create_guid())
+    merchant_id = settings.PAYBOX['MERCHANT_ID']
+    secret_key = settings.PAYBOX['SECRET_KEY']
+    payment_url = settings.PAYBOX['PAYMENT_URL']
+    result_url = settings.PAYBOX['RESULT_URL']
+    success_url = settings.PAYBOX['SUCCESS_URL']
+    failure_url = settings.PAYBOX['FAILURE_URL']
+    testing_mode = settings.PAYBOX['TESTING_MODE']
+    script = urlparse(payment_url).path[1:]
+    salt = settings.SECRET_KEY
 
     params = {
-        'pg_merchant_id':       merchant_id,
-        'pg_amount':            amount,
-        'pg_description':       description,
-        'pg_order_id':          order_id,
-        'pg_salt':              salt,
-        'pg_result_url':        result_url,
-        'pg_success_url':       success_url,
-        'pg_failure_url':       failure_url,
-        'pg_testing_mode':      testing_mode,
+        'pg_merchant_id': merchant_id,
+        'pg_amount': amount,
+        'pg_description': description,
+        'pg_order_id': order_id,
+        'pg_salt': salt,
+        'pg_result_url': result_url,
+        'pg_success_url': success_url,
+        'pg_failure_url': failure_url,
+        'pg_testing_mode': testing_mode,
     }
     params['pg_sig'] = sign(script, secret_key, params)
 
@@ -79,15 +79,15 @@ def get_checkout_url(parameters):
 def get_order_status(order_id):
 
     merchant_id = settings.PAYBOX['MERCHANT_ID']
-    secret_key  = settings.PAYBOX['SECRET_KEY']
-    status_url  = settings.PAYBOX['STATUS_URL']
-    script      = urlparse(status_url).path[1:]
-    salt        = settings.SECRET_KEY
+    secret_key = settings.PAYBOX['SECRET_KEY']
+    status_url = settings.PAYBOX['STATUS_URL']
+    script = urlparse(status_url).path[1:]
+    salt = settings.SECRET_KEY
 
     params = {
-        'pg_merchant_id':   merchant_id,
-        'pg_order_id':      order_id,
-        'pg_salt':          salt,
+        'pg_merchant_id': merchant_id,
+        'pg_order_id': order_id,
+        'pg_salt': salt,
     }
     params['pg_sig'] = sign(script, secret_key, params)
 
@@ -98,7 +98,7 @@ def get_order_status(order_id):
 
 def set_callback(request):
 
-    if (request.method == 'POST'):
+    if request.method == 'POST':
         # convert query string to dictionary
         data = dict(parse_qsl(request.body.decode('utf-8')))
         # get transaction result
--- src/carbase/helpers.py
+++ src/carbase/helpers.py
@@ -2,7 +2,6 @@ from django.http import HttpResponse
 from django.conf import settings
 
 from urllib.request import Request, urlopen
-import hashlib
 import uuid
 import json
 
@@ -13,24 +12,7 @@ Generates HTTP Response
 
 
 def http_code(message, status):
-    response = HttpResponse(message, status=status)
-    response['Access-Control-Allow-Credentials'] = 'true'
-    return response
-
-
-'''
-creates signature based on password string + optional parameters
-concatenates in alphabetic order and separated by | symbol
-converts string to hash SHA-1
-'''
-
-
-def create_signature(merchant_password, parameters):
-    params = str(merchant_password)
-    for key, value in sorted(parameters.items()):
-        params += '|{}'.format(value)
-    buffer = params.encode('utf-8')
-    return str(hashlib.sha1(buffer).hexdigest())
+    return HttpResponse(message, status=status)
 
 
 '''
@@ -75,4 +57,3 @@ converts request body to json
 def request_to_json(request):
     data = json.loads(request.body.decode('utf-8'))
     return data
-
