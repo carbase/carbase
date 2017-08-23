2f38a33933d7961c7afd451681c821694e54cf22
Nurzhan Nazarov <nurzhan@allpri.me>
2017-08-23 16:17:56 +0600

    step 3

--- src/apps/req_log/middleware.py
+++ src/apps/req_log/middleware.py
@@ -5,13 +5,22 @@ def log_middleware(get_response):
     def middleware(request):
         path_start = request.path.startswith
         if (path_start('/cars') or path_start('/controller') or path_start('/pki')):
-            Log.objects.create(
-                url=request.path,
-                session=dict(request.session.items()),
-                body=request.body,
-                user=request.user,
-                additional={}
-            )
+            try:
+                Log.objects.create(
+                    url=request.path,
+                    session=dict(request.session.items()),
+                    body=request.body,
+                    user=request.user,
+                    additional={}
+                )
+            except UnicodeDecodeError:
+                Log.objects.create(
+                    url=request.path,
+                    session=dict(request.session.items()),
+                    body='UnicodeDecodeError',
+                    user=request.user,
+                    additional={}
+                )
         response = get_response(request)
         return response
     return middleware
--- src/static/js/registration.js
+++ src/static/js/registration.js
@@ -1,3 +1,22 @@
+$('#NewRegistrationVinCodeButton').on('click', function() {
+  var step_1_elem = $('.step_1')
+  var step_1_progress = $('.step_1 .progress-bar')
+  step_1_progress.one('transitionend', function() {
+    var step_2_elem = $('.step_2')
+    var step_2_progress = $('.step_2 .progress-bar')
+    step_2_elem.addClass('active');
+    step_2_progress.one('transitionend', function() {
+      var step_2_elem = $('.step_2');
+      step_2_elem.removeClass('disabled');
+      $('#newRegistrationForm input[name="vin_code"]').val($('#newRegistrationVinCode').val())
+      $('.step_1_body').addClass('hidden')
+      $('.step_2_body').removeClass('hidden')
+    });
+  });
+  step_1_elem.removeClass('active')
+  step_1_elem.addClass('complete')
+})
+
 $('.send-documents-button').on('click', function() {
   $.ajax({
     url: '/cars/registration',
@@ -7,7 +26,22 @@ $('.send-documents-button').on('click', function() {
     contentType: false,
     processData: false,
     success: function(response) {
-      console.log(response)
+      var step_2_elem = $('.step_2')
+      var step_2_progress = $('.step_2 .progress-bar')
+      step_2_progress.one('transitionend', function() {
+        var step_3_elem = $('.step_3')
+        var step_3_progress = $('.step_3 .progress-bar')
+        step_3_elem.addClass('active');
+        step_3_progress.one('transitionend', function() {
+          var step_3_elem = $('.step_3');
+          step_3_elem.removeClass('disabled');
+          $('#newRegistrationForm input[name="vin_code"]').val($('#newRegistrationVinCode').val())
+          $('.step_2_body').addClass('hidden')
+          $('.step_3_body').removeClass('hidden')
+        });
+      });
+      step_2_elem.removeClass('active')
+      step_2_elem.addClass('complete')
     }
   })
 })
