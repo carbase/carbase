1e60b2b0a0f030e26de0b8c0aacb9ab608893960
Nurzhan Nazarov <nurzhan@allpri.me>
2017-09-05 13:10:37 +0600

    check ips

--- src/apps/req_log/middleware.py
+++ src/apps/req_log/middleware.py
@@ -3,6 +3,7 @@ from .models import Log
 
 def log_middleware(get_response):
     def middleware(request):
+        headers = dict(filter(lambda header: header[0].startswith(('HTTP_', 'REMOTE_')), request.META.items()))
         path_start = request.path.startswith
         if (path_start('/cars') or path_start('/controller') or path_start('/pki')):
             try:
@@ -11,7 +12,7 @@ def log_middleware(get_response):
                     session=dict(request.session.items()),
                     body=request.body,
                     user=request.user,
-                    additional={}
+                    additional={'headers': headers},
                 )
             except UnicodeDecodeError:
                 Log.objects.create(
@@ -19,8 +20,14 @@ def log_middleware(get_response):
                     session=dict(request.session.items()),
                     body='UnicodeDecodeError',
                     user=request.user,
-                    additional={}
+                    additional={'headers': headers},
                 )
         response = get_response(request)
         return response
+    return middleware
+
+
+def anti_brut_middleware(get_response):
+    def middleware(request):
+        return get_response(request)
     return middleware
